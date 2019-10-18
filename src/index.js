be9a3a513b5cbbe7f5e6ce95f60ad18fe16366aa
littleFFFFFF <853247919@qq.com>
2019-10-18 17:26:46 +0800

    create typescript app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.js';
-import ImageShow from './routes/imageShow/index.tsx';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<ImageShow />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- typing.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.css';
-declare module "*.png";
-declare module "*.svg";
-declare module "*.png";
-declare module "*.jpg";
-declare module "*.less";
-declare module "*.ts";
